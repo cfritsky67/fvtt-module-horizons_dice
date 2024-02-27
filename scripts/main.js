ed3fb7360b460da7c288f114ca08d0e132822c69
cfritsky67 <137589463+cfritsky67@users.noreply.github.com>
2024-02-27 15:22:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ Hooks.once('diceSoNiceInit', (dice3d) => {
     dice3d.addTexture("Clueless", {
         name: "☄️Clueless",
         composite: "source-over",
-        source: "modules/horizon_dice/horizons-dice/textures/clueless.webm",
-        bump:"modules/horizon_dice/horizons-dice/textures/clueless.webm"
+        source: "modules/horizon_dice/horizons-dice/textures/animated/clueless.webm",
+        bump:"modules/horizon_dice/horizons-dice/textures/animated/clueless.webm"
     });
 
 });
