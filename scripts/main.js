a025fa758c9c4dfb62fb9e492af18a42650b4613
cfritsky67 <137589463+cfritsky67@users.noreply.github.com>
2024-02-27 15:02:19 -0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+Hooks.once('diceSoNiceInit', (dice3d) => {
+    dice3d.addTexture("Beans", {
+        name: "Beans",
+        composite: "source-over",
+        source: "modules/bean-dice/images/beans.png",
+        bump:"modules/bean-dice/images/beans.png"
+    });
+
+});
