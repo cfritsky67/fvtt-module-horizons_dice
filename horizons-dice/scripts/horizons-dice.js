1021bbf2d3029b469c7281d0de10bc7453b4fdd5
cfritsky67 <137589463+cfritsky67@users.noreply.github.com>
2024-02-27 17:15:03 -0500

    Update and rename horizons-dice.json to horizons-dice.js

---
+++ horizons-dice/scripts/horizons-dice.js
@@ -0,0 +1,12 @@
+Hooks.on('diceSoNiceReady', (dice3d) => {
+
+	dice3d.addTexture("Clueless", {
+	    name: "Clueless",
+	    composite: "multiply",
+	    source: "modules/horizons-dice/textures/stills/clueless.webp",
+	    bump: "modules/horizons-dice/textures/stills/clueless.webp"
+	});
+
+
+
+});
