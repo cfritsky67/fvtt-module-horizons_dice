4a2786e3d2cd2983d29056fe8d70bb0c1ae7608a
cfritsky67 <137589463+cfritsky67@users.noreply.github.com>
2024-02-27 17:50:05 -0500

    Update horizons-dice.js

--- horizons-dice/scripts/horizons-dice.js
+++ horizons-dice/scripts/horizons-dice.js
@@ -1,10 +1,10 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
 	dice3d.addTexture("Clueless", {
-	    name: "☄️ Clueless",
+	    name: "HDC - Clueless",
 	    composite: "multiply",
-	    source: "modules/horizons-dice/textures/stills/clueless.webm",
-	    bump: "modules/horizons-dice/textures/stills/clueless.webm"
+	    source: "modules/horizons-dice/textures/animated/clueless.webm",
+	    bump: "modules/horizons-dice/textures/animated/clueless.webm"
 	});
 
 
